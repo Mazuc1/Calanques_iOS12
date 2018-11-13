abfd713a0ed0c5db720578e96e80ef638a01576d
Loic <loicmazuc@macbook-pro-de-loic.local>
2018-11-13 10:13:11 +0100

    Initial Commit

---
+++ Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Calanques
+//
+//  Created by Loic Mazuc on 13/11/2018.
+//  Copyright © 2018 Loic Mazuc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
